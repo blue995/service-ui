b4a5d2192de7aa54f4d701e2a9b49c03e8727f32
Tobias Blaufuß <t.blaufuss@gmx.de>
2020-01-27 23:45:07 +0100

    [UPDATE] init-repo script

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+CURRENT_DIR=.
+BASEDIR=$(dirname "$0")
+
+cd "$BASEDIR"/..
 git remote add main https://github.com/reportportal/service-api.git
 git fetch main
 git checkout -t main/master
 git checkout service-ui-with-tfs
+cd "$CURRENT_DIR"
