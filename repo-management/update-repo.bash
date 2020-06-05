aab9c4d9de5c3ef8ee94aa670c10b329ca2aac7a
Tobias Blaufuß <t.blaufuss@gmx.de>
2020-06-06 01:40:29 +0200

    [UPDATE] repo scripts

--- repo-management/update-repo.bash
+++ repo-management/update-repo.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+TAG="$1"
+
+if [ -z "$TAG" ]
+then
+    echo "Tag to merge with is missing"
+    exit 1
+fi
+
 git checkout master
 git pull
 git checkout service-ui-with-tfs
-git merge master
+git merge "$TAG"
